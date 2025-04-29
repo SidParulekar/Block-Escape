12718720e5c22ef5f14bbf8ae0e4c25ed7ce15ed
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-04-29 12:07:25 +0530

    Made Block as prefab, Created Player script and made build

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+
+    [SerializeField] private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        MovePlayer();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //Debug.Log($"Horizontal: {horizontalInput}, Vertical: {verticalInput}");
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0);
+        Vector3 moveAmount = moveDirection.normalized * speed * Time.deltaTime;
+        transform.position += moveAmount;
+    }
+}
